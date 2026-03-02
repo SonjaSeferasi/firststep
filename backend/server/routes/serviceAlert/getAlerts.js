c49f545579cb289d6b340bd2a5f303b3df9a2bb9
dlinhngph <dlinhngph2305@gmail.com>
2026-03-01 21:03:10 -0500

    update error

---
+++ backend/server/routes/serviceAlert/getAlerts.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const ServiceAlert = require("../../models/serviceAlerts");
+
+// GET /api/alerts
+router.get("/", async (req, res) => {
+  try {
+    const alerts = await ServiceAlert.find();
+    res.status(200).json(alerts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+module.exports = router;
