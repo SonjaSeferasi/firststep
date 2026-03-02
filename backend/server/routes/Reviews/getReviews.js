653e81c9456de6ef8da6674fff9f58404dd52c09
SonjaSeferasi <sonjaseferasi15@gmail.com>
2026-03-01 21:03:00 -0500

    changing a small typo on the name

--- backend/server/routes/Reviews/getReviews.js
+++ backend/server/routes/Reviews/getReviews.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const StationReviews = require("../../models/StationReview");
@@ -7,7 +6,7 @@ router.get("/", async (req, res) => {
 
   try {
 
-    const reviews = await StationReviews
+    const reviews = await StationReview
       .find()
       .sort({ createdAt: -1 });
 
@@ -25,42 +24,3 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
-=======
-router.get("/", async (req, res) => {
-
-    try {
-
-        const { rating, sort } = req.query;
-
-        let query = {};
-
-        // Filter by rating
-        if (rating) {
-            query.rating = rating;
-        }
-
-        let reviews = Review.find(query);
-
-        // Sorting
-        if (sort === "newest") {
-            reviews = reviews.sort({ createdAt: -1 });
-        }
-
-        if (sort === "highest") {
-            reviews = reviews.sort({ rating: -1 });
-        }
-
-        const result = await reviews.populate("userId", "name");
-
-        res.status(200).json(result);
-
-    } catch (error) {
-
-        res.status(500).json({
-            message: "Server error"
-        });
-
-    }
-
-});
->>>>>>> 1d35c573bee043edf788458b844d5fb2d8c23498
